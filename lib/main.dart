d30b730758e4c31eead9310558e03d808fa81b46
BetsegawLire <betsegawhazard@gmail.com>
2023-10-05 16:26:55 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './signUp.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SignUpScreen(),
+    theme: ThemeData(useMaterial3: true),
+  ));
+}
---
+++ lib/signUp.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  bool _isButtonDisabled = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _firstNameController.addListener(_checkButtonStatus);
+  }
+
+  void _checkButtonStatus() {
+    setState(() {
+      _isButtonDisabled = _firstNameController.text.isEmpty;
+    });
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text("Sign Up"),
+        actions: [
+          Tooltip(
+            message: "Your real first name is required to create a profile.",
+            child: IconButton(
+              icon: const Icon(Icons.help),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Align(
+        alignment: Alignment.bottomRight,
+        child: FloatingActionButton(
+          backgroundColor: _isButtonDisabled ? Colors.grey : Colors.blue,
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 16),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "What is your name?",
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _firstNameController,
+              decoration: const InputDecoration(
+                hintText: 'Enter your first name',
+                labelText: 'First Name',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _lastNameController,
+              decoration: const InputDecoration(
+                hintText: 'Enter your last name(Optional)',
+                labelText: 'Last Name(optional)',
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+}
